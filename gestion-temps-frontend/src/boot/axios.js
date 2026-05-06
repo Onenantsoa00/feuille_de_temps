8aaabec50dbcdee0616a35e3e11d5af704e24a4c
Onenantsoa00 <ranaivoonenantsoa@gmail.com>
2026-05-06 11:51:47 +0300

    fix: API URL production

--- gestion-temps-frontend/src/boot/axios.js
+++ gestion-temps-frontend/src/boot/axios.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 
 const api = axios.create({
   //baseURL: "http://192.168.16.110:3000/api",
-  baseURL: 'http://localhost:3000/api',
+  //baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://feuille-de-temps.onrender.com/api'
 })
 
 api.interceptors.request.use((config) => {
--- gestion-temps-frontend/src/boot/socket.js
+++ gestion-temps-frontend/src/boot/socket.js
@@ -1,7 +1,7 @@
 import { boot } from 'quasar/wrappers'
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:3000')
+const socket = io('https://feuille-de-temps.onrender.com');
 
 export default boot(({ app }) => {
   app.config.globalProperties.$socket = socket
